3fb6413bf1f8f665155ddcef2f657bfcf0720e53
63832 <63832@gymnasium.ax>
2025-10-28 09:07:06 +0200

    fixat start_url och scope

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/~63832/weather',
+    base: '/~s63832/weather',
     plugins: [
         vue(),
         vueDevTools(),
